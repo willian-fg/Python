323776871c2525f36630b45da468aedeea6892f9
willian-fg <willflorgasp@gmail.com>
2026-05-21 07:37:41 -0300

    Correções no robo de triagem

--- Exercicios/robos_de_triagem.py
+++ Exercicios/robos_de_triagem.py
@@ -8,30 +8,35 @@ class RoboDeTriagem:
     def processar_esteira(self, lista_de_pesos):
 
         self.lista_de_pesos = []
-        
-        for peso in lista_de_pesos:
-        
-                resto_da_divisão_do_peso = peso % 2
-                if (peso >= 100) and (peso <= 300):
-                    if resto_da_divisão_do_peso == 0:
-                        print(f"Peso {peso}, Aprovado")
-                        self.total_de_pecas_aprovadas = self.total_de_pecas_aprovadas + 1
-                        self.peso_total_processado = self.peso_total_processado + peso
 
-                elif (peso == 99) or (peso == 399):
-                    print("Alerta, Peso crítico detectado. Travando esteira!")
-                    break
+        for peso in lista_de_pesos:
+
+            resto_da_divisão_do_peso = peso % 2
+            if (peso >= 100) and (peso <= 300):
+                if resto_da_divisão_do_peso == 0:
+                    print(f"Peso {peso}, Aprovado")
+                    self.total_de_pecas_aprovadas += 1
+                    self.peso_total_processado += peso
                 
                 else:
-                    print("Pesso {peso} reprovado.")
-                    self.total_de_pecas_reprovadas = self.total_de_pecas_reprovadas + 1
-                    self.peso_total_processado = self.peso_total_processado + peso
-        
+                    print(f"Peso {peso} reprovado (é ímpar).")
+                    self.total_de_pecas_reprovadas += 1
+                    self.peso_total_processado += peso
+
+            elif (peso == 99) or (peso == 333):
+                    print("Alerta, Peso crítico detectado. Travando esteira!")
+                    break
+
+            else:
+                print("Pesso {peso} reprovado.")
+                self.total_de_pecas_reprovadas += 1
+                self.peso_total_processado += peso
+
 
     def relatorio(self):
         print(f"""
-        Total de Peças Aprovadas: {self.total_de_pecas_reprovadas}\n
-        Trotal de Peças Reprovadas {self.total_de_pecas_reprovadas}\n
+        Total de Peças Aprovadas: {self.total_de_pecas_aprovadas}
+        Trotal de Peças Reprovadas {self.total_de_pecas_reprovadas}
         Total de Peso Processado {self.peso_total_processado}
         """)
 
