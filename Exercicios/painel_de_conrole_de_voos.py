fb919ad4198ac982eb3827d22e74e248c9e9f630
willian-fg <willflorgasp@gmail.com>
2026-05-21 14:54:57 -0300

    Exercicio Painel de Aeroporto finalizado

--- Exercicios/painel_de_conrole_de_voos.py
+++ Exercicios/painel_de_conrole_de_voos.py
@@ -1,5 +1,5 @@
 class Avião:
-    def __init__(self, codigo, destino, capacidade, passageiros_abordados):
+    def __init__(self, codigo, destino, capacidade):
         self.codigo = codigo
         self.destino = destino
         self.capacidade = capacidade
@@ -16,5 +16,35 @@ class PainelAeroporto:
     def embarcar_passageiros(self, codigo_de_voo, quantidade):
         for aviao in self.voos_agendados:
             if aviao.codigo == codigo_de_voo:
-                if 
+                if aviao.passageiros_abordados + quantidade > aviao.capacidade:
+                    print(f"Erro: O voo {aviao.codigo} excedeu a capacidade")
+                else:
+                    aviao.passageiros_abordados += quantidade
 
+
+    def exibir_painel_geral(self):
+        print(" ----- Painel Geral -----")
+        for aviao in self.voos_agendados:
+            print( f"Voo: {aviao.codigo} | Destino: {aviao.destino} | Passageiros: {aviao.passageiros_abordados}")
+
+
+# ----- voos ---------
+
+aviao1 = Avião("AZU-4412", "Rio de Janeiro", 150)
+aviao2 = Avião("TAM-3301", "Salvador", 200)
+
+aeroporto = PainelAeroporto()
+
+aeroporto.registrar_voo(aviao1)
+aeroporto.registrar_voo(aviao2)
+
+# ----- Embarques 
+
+print(" ----- Iniciando Embarques -----")
+aeroporto.embarcar_passageiros("AZU-4412", 120)
+aeroporto.embarcar_passageiros("TAM-3301", 250)
+
+# ----- Painel
+
+print("\n === PAINEL DE EMBARQUE DE VOOS === ")
+aeroporto.exibir_painel_geral()
